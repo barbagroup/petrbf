af96cff72bab470a7d3f98d3c7970b33e68bb664
rioyokota <rioyokota@5f73882e-ad0c-11de-b7bf-31c12d054fdc>
2009-10-06 12:52:09 +0000

    Initial upload

---
+++ trunk/batch2.sh
@@ -0,0 +1,7 @@
+export NP=1
+while [ $NP -le 1024 ]
+do
+echo $NP
+llsubmit bluegene.sh
+export NP=`echo $NP \* 2 | bc`
+done
---
+++ trunk/get_vorticity.h
@@ -0,0 +1,25 @@
+class Get_vorticity
+{
+  int i,j,ic,il,ista,iend;
+  double w,dx,dy;
+public:
+  void get_vorticity(PARTICLE *particle,CLUSTER *cluster)
+  {
+    for (ic=cluster->icsta; ic<cluster->icend; ic++) {
+      Get_trunc trunc;
+      trunc.get_trunc(particle,cluster,ic);
+      ista = cluster->ista[ic];
+      iend = cluster->iend[ic];
+      for (i=ista; i<=iend; i++) {
+        w = 0;
+        for (j=0; j<cluster->nptruncj; j++) {
+          dx = particle->xil[i]-cluster->xjt[j];
+          dy = particle->yil[i]-cluster->yjt[j];
+          w += cluster->gjt[j]*exp(-(dx*dx+dy*dy)/(2*particle->sigma*particle->sigma))/
+            (2*pi*particle->sigma*particle->sigma);
+        }
+        particle->wil[i] = w;
+      }
+    }
+  }
+};
